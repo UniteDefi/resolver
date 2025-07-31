4bbaa5cddf62cb6fbd10ed5e9659eac8b1f303e9
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-07-31 23:09:48 +0700

    Update contracts/evm/script/DeployTokens.s.sol

--- contracts/evm/script/DeployTokens.s.sol
+++ contracts/evm/script/DeployTokens.s.sol
@@ -4,7 +4,8 @@ pragma solidity ^0.8.23;
 import {Script} from "forge-std/Script.sol";
 import {console} from "forge-std/console.sol";
 import {MockWrappedNative} from "../src/mocks/MockWrappedNative.sol";
-import {MockERC20} from "../src/mocks/MockERC20.sol";
+import {MockUSDT} from "../src/mocks/MockUSDT.sol";
+import {MockDAI} from "../src/mocks/MockDAI.sol";
 import {ChainConfig} from "./ChainConfig.sol";
 
 contract DeployTokens is Script {
@@ -13,18 +14,25 @@ contract DeployTokens is Script {
         address usdt;
         address dai;
     }
-    
+
     function run() external returns (TokenDeployment memory deployment) {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         address deployer = vm.addr(deployerPrivateKey);
-        
+
         ChainConfig config = new ChainConfig();
-        ChainConfig.ChainInfo memory chainInfo = config.getChainInfo(block.chainid);
-        
-        console.log("Deploying tokens on", chainInfo.name, "with deployer:", deployer);
-        
+        ChainConfig.ChainInfo memory chainInfo = config.getChainInfo(
+            block.chainid
+        );
+
+        console.log(
+            "Deploying tokens on",
+            chainInfo.name,
+            "with deployer:",
+            deployer
+        );
+
         vm.startBroadcast(deployerPrivateKey);
-        
+
         // Deploy Wrapped Native Token
         MockWrappedNative wrappedNative = new MockWrappedNative(
             chainInfo.wrappedNativeName,
@@ -32,19 +40,19 @@ contract DeployTokens is Script {
         );
         deployment.wrappedNative = address(wrappedNative);
         console.log("Wrapped Native deployed at:", deployment.wrappedNative);
-        
+
         // Deploy Mock USDT
-        MockERC20 usdt = new MockERC20("Mock USDT", "USDT", 6);
+        MockUSDT usdt = new MockUSDT("Mock USDT", "USDT", 6);
         deployment.usdt = address(usdt);
         console.log("Mock USDT deployed at:", deployment.usdt);
-        
+
         // Deploy Mock DAI
-        MockERC20 dai = new MockERC20("Mock DAI", "DAI", 18);
+        MockDAI dai = new MockDAI("Mock DAI", "DAI", 18);
         deployment.dai = address(dai);
         console.log("Mock DAI deployed at:", deployment.dai);
-        
+
         vm.stopBroadcast();
-        
+
         return deployment;
     }
 }
