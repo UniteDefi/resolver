a193d3c17dbab3ff1b676f86792c439f23d81e5b
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-02 01:47:17 +0700

    Remove contracts/cardano/tests/setup.ts

--- contracts/cardano/tests/setup.ts
+++
@@ -1,8 +0,0 @@
-import dotenv from "dotenv";
-import path from "path";
-
-// Load environment variables
-dotenv.config({ path: path.resolve(__dirname, "../.env") });
-
-// Set test timeout
-jest.setTimeout(60000);
