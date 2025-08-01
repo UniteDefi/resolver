03d4da4f1b137bcf49eea1a7195628e0f94abcd9
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-01 14:56:32 +0700

    Update contracts/evm/script/VerifyContracts.sh

--- contracts/evm/script/VerifyContracts.sh
+++ contracts/evm/script/VerifyContracts.sh
@@ -69,7 +69,7 @@ echo -e "\nVerifying Arbitrum Sepolia contracts..."
 forge verify-contract \
     --chain-id 421614 \
     --constructor-args $(cast abi-encode "constructor(string,string)" "Wrapped Ether" "WETH") \
-    --etherscan-api-key $ARBISCAN_API_KEY \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
     --compiler-version v0.8.23+commit.f704f362 \
     0x3f00f779Aa61f9068A94A526526733bEBee7569A \
     src/mocks/MockWrappedNative.sol:MockWrappedNative
@@ -78,7 +78,7 @@ forge verify-contract \
 forge verify-contract \
     --chain-id 421614 \
     --constructor-args $(cast abi-encode "constructor(string,string,uint8)" "Mock USDT" "USDT" 6) \
-    --etherscan-api-key $ARBISCAN_API_KEY \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
     --compiler-version v0.8.23+commit.f704f362 \
     0xea6392e0a0cac24525d9CFc5D4763C79c558342C \
     src/mocks/MockUSDT.sol:MockUSDT
@@ -87,7 +87,7 @@ forge verify-contract \
 forge verify-contract \
     --chain-id 421614 \
     --constructor-args $(cast abi-encode "constructor(string,string,uint8)" "Mock DAI" "DAI" 18) \
-    --etherscan-api-key $ARBISCAN_API_KEY \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
     --compiler-version v0.8.23+commit.f704f362 \
     0xd17384bEBE94Ff08D9231e31D2c5F1a322405a52 \
     src/mocks/MockDAI.sol:MockDAI
@@ -96,7 +96,7 @@ forge verify-contract \
 forge verify-contract \
     --chain-id 421614 \
     --constructor-args "" \
-    --etherscan-api-key $ARBISCAN_API_KEY \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
     --compiler-version v0.8.23+commit.f704f362 \
     0x75A3FB7529830deFBBe3EC582ca34508aDcA89Ae \
     src/one-inch/LimitOrderProtocol.sol:LimitOrderProtocol
@@ -105,7 +105,7 @@ forge verify-contract \
 forge verify-contract \
     --chain-id 421614 \
     --constructor-args $(cast abi-encode "constructor(address,address,address,address,uint256,uint256)" "0x75A3FB7529830deFBBe3EC582ca34508aDcA89Ae" "0xea6392e0a0cac24525d9CFc5D4763C79c558342C" "0xea6392e0a0cac24525d9CFc5D4763C79c558342C" "0x5121aA62b1f0066c1e9d27B3b5B4E64e0c928a35" 1800 1800) \
-    --etherscan-api-key $ARBISCAN_API_KEY \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
     --compiler-version v0.8.23+commit.f704f362 \
     0x1BEBC69b153363525d08408C9d73B23d83BE0ae8 \
     src/UniteEscrowFactory.sol:UniteEscrowFactory
@@ -114,7 +114,7 @@ forge verify-contract \
 forge verify-contract \
     --chain-id 421614 \
     --constructor-args $(cast abi-encode "constructor(address,address,address)" "0x1BEBC69b153363525d08408C9d73B23d83BE0ae8" "0x75A3FB7529830deFBBe3EC582ca34508aDcA89Ae" "0x5121aA62b1f0066c1e9d27B3b5B4E64e0c928a35") \
-    --etherscan-api-key $ARBISCAN_API_KEY \
+    --etherscan-api-key $ETHERSCAN_API_KEY \
     --compiler-version v0.8.23+commit.f704f362 \
     0x45ca8Bf57269D128b111C145c4fAE673c06eF163 \
     src/SimpleResolver.sol:SimpleResolver
