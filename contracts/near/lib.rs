4c7b462e9f440cae6ed8734405a44be29b10df1e
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-01 10:53:15 +0700

    Remove contracts/near/lib.rs

--- contracts/near/lib.rs
+++
@@ -1,5 +0,0 @@
-#[cfg(all(target_arch = "wasm32", not(feature = "no-contract")))]
-pub mod simple_relayer;
-
-#[cfg(all(target_arch = "wasm32", not(feature = "no-contract")))]
-pub mod simple_escrow;
