543323c62da801146de3cd6e8970ac987c174e65
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-03 06:13:55 +0700

    Add contracts/osmosis/contracts/unite-resolver/

---
+++ contracts/osmosis/contracts/unite-resolver/src/contract.rs
@@ -0,0 +1,28 @@
+use cosmwasm_std::{
+    to_json_binary, Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult,
+};
+
+use crate::error::ContractError;
+use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+
+pub fn instantiate(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: InstantiateMsg,
+) -> Result<Response, ContractError> {
+    Ok(Response::new().add_attribute("method", "instantiate"))
+}
+
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: ExecuteMsg,
+) -> Result<Response, ContractError> {
+    Ok(Response::new().add_attribute("method", "execute"))
+}
+
+pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
+    to_json_binary(&"query")
+}
