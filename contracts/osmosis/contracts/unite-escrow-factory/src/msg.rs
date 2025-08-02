b6925929d3a8439127c4ef2eecb729748bca60f7
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-03 06:13:02 +0700

    Add contracts/osmosis/contracts/unite-escrow-factory/

---
+++ contracts/osmosis/contracts/unite-escrow-factory/src/msg.rs
@@ -0,0 +1,20 @@
+use schemars::JsonSchema;
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub struct InstantiateMsg {}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum ExecuteMsg {
+    CreateEscrow {
+        recipient: String,
+        amount: String,
+        timeout: u64,
+    },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+#[serde(rename_all = "snake_case")]
+pub enum QueryMsg {}
