e0f256a2e026425875c8c5dcd428da85e50b8a89
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-03 21:42:45 +0700

    Update contracts/osmosis/contracts/unite-resolver/src/state.rs

--- contracts/osmosis/contracts/unite-resolver/src/state.rs
+++ contracts/osmosis/contracts/unite-resolver/src/state.rs
@@ -1,11 +1,15 @@
 use cosmwasm_std::Addr;
-use cw_storage_plus::Item;
+use cw_storage_plus::{Item, Map};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 pub struct Config {
     pub owner: Addr,
+    pub factory: Addr,
+    pub order_protocol: Addr,
 }
 
 pub const CONFIG: Item<Config> = Item::new("config");
+pub const ORDER_FILLS: Map<String, u128> = Map::new("order_fills");
+pub const ESCROW_ADDRESSES: Map<String, Addr> = Map::new("escrow_addresses");
