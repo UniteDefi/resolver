2b881fd7e7ed1b5eb7ebba24351fe7433ffd71fa
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-03 22:48:48 +0700

    Update contracts/xrp/src/resolver/index.ts

--- contracts/xrp/src/resolver/index.ts
+++ contracts/xrp/src/resolver/index.ts
@@ -1,4 +1,5 @@
 export { XRPLResolver } from "./XRPLResolver";
 export { XRPLResolverManager } from "./XRPLResolverManager";
 export { CrossChainCoordinator } from "./CrossChainCoordinator";
+export { XRPLUniteResolver } from "./XRPLUniteResolver";
 export * from "./types";
