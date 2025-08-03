423d6caea55445b6a5704e4d6e75c0dd76c6986d
Gabriel Antony Xaviour <gabrielantony56@gmail.com>
2025-08-03 21:40:25 +0700

    Update contracts/osmosis/contracts/unite-resolver/src/msg.rs

--- contracts/osmosis/contracts/unite-resolver/src/msg.rs
+++ contracts/osmosis/contracts/unite-resolver/src/msg.rs
@@ -1,19 +1,71 @@
+use cosmwasm_std::{Addr, Uint128};
 use schemars::JsonSchema;
 use serde::{Deserialize, Serialize};
+use crate::types::{Order, Immutables};
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub struct InstantiateMsg {}
+pub struct InstantiateMsg {
+    pub factory: Addr,
+    pub order_protocol: Addr,
+}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
 pub enum ExecuteMsg {
-    ResolveTransaction {
-        tx_hash: String,
-        chain_id: String,
+    DeploySrc {
+        immutables: Immutables,
+        order: Order,
+        signature: String,
+        amount: Uint128,
+    },
+    DeploySrcPartial {
+        immutables: Immutables,
+        order: Order,
+        signature: String,
+        amount: Uint128,
+        partial_amount: Uint128,
+    },
+    DeployDst {
+        immutables: Immutables,
+        src_cancellation_timestamp: u64,
+    },
+    DeployDstPartial {
+        immutables: Immutables,
+        src_cancellation_timestamp: u64,
+        partial_amount: Uint128,
+    },
+    FillOrder {
+        immutables: Immutables,
+        order: Order,
+        src_cancellation_timestamp: u64,
+        src_amount: Uint128,
+    },
+    ApproveToken {
+        token: String,
+        amount: Uint128,
+    },
+    Withdraw {
+        escrow: Addr,
+        secret: String,
+        immutables: Immutables,
+    },
+    Cancel {
+        escrow: Addr,
+        immutables: Immutables,
     },
 }
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
-pub enum QueryMsg {}
+pub enum QueryMsg {
+    GetConfig {},
+    GetOrderHash { order: Order },
+}
+
+#[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
+pub struct ConfigResponse {
+    pub owner: Addr,
+    pub factory: Addr,
+    pub order_protocol: Addr,
+}
